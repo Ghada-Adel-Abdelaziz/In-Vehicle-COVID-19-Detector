ff481b871717ea3aca3ea2a563b11ee7518effa5
Toqa20 <toaa.hakim22@gmail.com>
2021-04-15 21:10:43 +0200

    I2C driver implementation completeness (to be reviewed)

--- I2C_Driver/src/I2C_lcfg.c
+++ I2C_Driver/src/I2C_lcfg.c
@@ -7,6 +7,40 @@
 
 #include "I2C_lcfg.h"
 #include "I2C_cfg.h"
+void LED1_ON(void)   // I2C TX complete
+{
+	static char i = 0;
+	i++;
+
+	if(i == 1)
+	{
+		GPIO_WriteOutputPin(ORANGE_LED,1);
+	}
+	else if(i == 2)
+	{
+		i = 0;
+		GPIO_WriteOutputPin(ORANGE_LED,0);
+	}
+
+}
+
+void LED2_ON(void)   // UART RX complete
+{
+	static char i = 0;
+	i++;
+
+	if(i == 1)
+	{
+		GPIO_WriteOutputPin(GREEN_LED,1);
+	}
+	else if(i == 2)
+	{
+		i = 0;
+		GPIO_WriteOutputPin(GREEN_LED,0);
+	}
+
+	Uart_SendDataAsync(USART2_,RX_Buffer,sizeof(RX_Buffer));
+}
 
 
 extern I2C_Config_t I2C_ConfigArray[NUMBER_OF_CONFIGURED_I2C]=
